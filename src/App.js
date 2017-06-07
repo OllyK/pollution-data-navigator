47a560bdf1bb96cf18acf6ddc5034dedabca5b91
Olly King <olly@zoetrope.io>
2017-06-07 19:00:17 +0100

    Measurement info is now displayed when clicking on button in table

--- src/App.js
+++ src/App.js
@@ -13,11 +13,13 @@ class App extends Component {
       hasErrored: false,
       isLoading: false,
       citiesList: {"results": []},
-      locationInfo: {"results": []}
+      locationInfo: {"results": []},
+      measurementInfo: {"results": [{"measurements": []}]}
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleMeasureChange = this.handleMeasureChange.bind(this);
     this.handleLocationClick = this.handleLocationClick.bind(this);
+    this.handleMeasureClick = this.handleMeasureClick.bind(this);
   }
 
   handleChange(e) {
@@ -29,13 +31,13 @@ class App extends Component {
   }
 
   handleLocationClick(e) {
-    this.fetchData('https://api.openaq.org/v1/locations?city[]=' + this.state.areaSelected, true)
+    this.fetchData('https://api.openaq.org/v1/locations?city[]=' + this.state.areaSelected, 'location')
   }
-   handleMeasureClick (e) {
-    alert(e.target.parentNode.getAttribute("value"));
+  handleMeasureClick (e) {
+    this.fetchData('https://api.openaq.org/v1/latest?location=' + e.target.parentNode.getAttribute("value"), 'measurements')
   }
 
-  fetchData(url, location) {
+  fetchData(url, type) {
     this.setState({ isLoading: true });
     fetch(url)
         .then((response) => {
@@ -47,16 +49,24 @@ class App extends Component {
         })
         .then((response) => response.json())
         .then((items) => {
-          if (location === false) {
-            this.setState({ citiesList: items })
+          switch (type) {
+            case 'cities':
+              this.setState({ citiesList: items });
+              break;
+            case 'location':
+              this.setState({ locationInfo: items });
+              break;
+            case 'measurements':
+              this.setState({ measurementInfo: items });
+              break;
           }
-          else {this.setState({ locationInfo: items })}
+
         })
         .catch(() => this.setState({ hasErrored: true }));
   }
 
   componentDidMount() {
-        this.fetchData('https://api.openaq.org/v1/cities?country=GB&limit=1000', false);
+        this.fetchData('https://api.openaq.org/v1/cities?country=GB&limit=1000', 'cities');
     }
 
 
@@ -95,6 +105,7 @@ class App extends Component {
                               locData={this.state.locationInfo.results}
                               onLocationClick={this.handleLocationClick}
                               onMeasurementClick={this.handleMeasureClick} />
+                <MeasurementOutput measurements={this.state.measurementInfo.results[0]}/>
               </div>
             );
   }
@@ -158,7 +169,7 @@ class LocationInfoDisplay extends Component {
           if (this.props.locData.length > 0) {
                     return (<div >
                             <br />
-                            <table className="Table">
+                            <table className="locationTable">
                               <tbody>
                                 <tr>
                                   <th>Location Name</th>
@@ -194,31 +205,46 @@ class CitySelector extends Component {
               Please Select
               </option>
             {this.props.results.map((item) => (
-
               <option value={item.city} key={item.city}
                       selected={this.props.area === item.city ? "selected" : null}>
                   {item.city}
               </option>
-
-
               ))}
           </select>
           );
   }
 }
 
-class DataOutput extends Component {
+class MeasurementOutput extends Component {
 
   render () {
+
+    if (this.props.measurements.measurements.length === 0){
+      return <div></div>
+    }
 
     return (
             <div>
               <p className="App-intro">
-                Here's the latest reading:
+                Here are the latest readings for {this.props.measurements.location}:
               </p>
-              <div className="Data">
-                  5 &#956;g/m<sup>3</sup>
-              </div>
+              <table className="locationTable">
+                  <tbody>
+                    <tr>
+                      <th>Measurement Type</th>
+                      <th>Reading</th>
+                      <th>Taken at</th>
+                    </tr>
+                    {this.props.measurements.measurements.map((item) => (
+                    <tr key={item.parameter} className="measurements">
+                        <td>{item.parameter.toUpperCase()}</td>
+                        <td>{item.value} {item.unit}</td>
+                        <td>{item.lastUpdated}</td>
+                    </tr>
+                    ))}
+                  </tbody>
+              </table>
+
             </div>
               );
   }
