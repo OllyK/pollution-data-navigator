016f8377576caeb47e965d63c0de0561df1c421d
Olly King <olly@zoetrope.io>
2017-06-08 10:14:47 +0100

    Added some action handlers and an itemsFetchData thunk

---
+++ src/actions/actions.js
@@ -0,0 +1,81 @@
+export function itemsHasErrored(bool) {
+    return {
+        type: 'ITEMS_HAS_ERRORED',
+        hasErrored: bool
+    };
+}
+
+export function itemsIsLoading(bool) {
+    return {
+        type: 'ITEMS_IS_LOADING',
+        isLoading: bool
+    };
+}
+
+export function citiesFetchDataSuccess(items) {
+    return {
+        type: 'CITIES_FETCH_DATA_SUCCESS',
+        citiesList: items
+    };
+}
+
+export function locationFetchDataSuccess(items) {
+    return {
+        type: 'LOCATION_FETCH_DATA_SUCCESS',
+        locationInfo: items
+    };
+}
+
+export function measurementFetchDataSuccess(items) {
+    return {
+        type: 'MEASUREMENT_FETCH_DATA_SUCCESS',
+        measurementInfo: items
+    };
+}
+
+export function areaSelected(value) {
+    return {
+        type: 'AREA_SELECTED',
+        areaSelected: value
+    };
+}
+
+export function measurementSelected(value) {
+    return {
+        type: 'MEASUREMENT_SELECTED',
+        measurementSelected: value
+    };
+}
+
+export function itemsFetchData(url, type) {
+    return (dispatch) => {
+        dispatch(itemsIsLoading(true));
+
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+
+                dispatch(itemsIsLoading(false));
+
+                return response;
+            })
+            .then((response) => response.json())
+            .then((items) => {
+            switch (type) {
+                case 'cities':
+                  dispatch(citiesFetchDataSuccess(items);
+                  break;
+                case 'location':
+                  dispatch(locationFetchDataSuccess(items);
+                  break;
+                case 'measurements':
+                  dispatch(measurementsFetchDataSuccess(items);
+                  break;
+          }
+
+        })
+            .catch(() => dispatch(itemsHasErrored(true)));
+    };
+}
