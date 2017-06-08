97b96efe572dcc0c84c36a75231f3bf9fdf57d79
Olly King <olly@zoetrope.io>
2017-06-08 11:11:47 +0100

    Configured index.js to use store and Provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
+
+import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore(); // You can also pass in an initialState here
+
+ReactDOM.render(
+                <Provider store={store}>
+                    <App />
+                </Provider>
+                document.getElementById('root')
+                );
 registerServiceWorker();
 
